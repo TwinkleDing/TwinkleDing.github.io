c600d99078c0ff2383432dd0b8229dbae523a000
dingyuliang <dingyuliang>
2022-04-25 17:50:54 +0800

    更新vue3+vite

---
+++ assets/index.c1471f25.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.ca41596a.js";import{o as r,i as o}from"./vendor.acbd4485.js";const c={};function n(t,a){return r(),o("div",null,"home")}var f=e(c,[["render",n]]);export{f as default};
