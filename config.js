5a2fb5ed642375b6f2d6ceb7dfff062beb2d1356
dingyuliang <dingyuliang>
2023-01-16 14:56:12 +0800

    更新代码

--- config.js
+++ config.js
@@ -2,6 +2,8 @@
     window.config = {
         title: "VUE3+TS",
         logo: "static/logo.png",
-        apiUrl: "http://192.168.1.0:6677"
+        devApiUrl: "http://localhost:6677",
+        proApiUrl: "http://192.168.1.0:6677",
+        ArLanuages: []
     }
 })()
