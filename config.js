c8f4a8bb4b015f3ba7cb0bdcb5e446b1adddf387
dingyuliang <dingyuliang>
2023-02-24 15:19:28 +0800

    更新一下apikey

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
         devApiUrl: "http://localhost:6677",
         proApiUrl: "http://192.168.1.0:6677",
         ArLanuages: [],
-        API_KEY: "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"
+        API_KEY: "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"
     }
 })()
