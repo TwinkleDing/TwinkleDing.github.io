077cb5f264be8461f248c2e7c5719a57873a10fb
dingyuliang <dingyuliang>
2023-02-24 15:50:29 +0800

    更新代码

--- assets/404_cloud.4ac75bad.js
+++
@@ -1,1 +0,0 @@
-var a="/static/png/404-538aa4d7.png",p="/static/png/404_cloud-98e7ac66.png";export{a as _,p as a};
--- config.js
+++ config.js
@@ -4,7 +4,6 @@
         logo: "static/logo.png",
         devApiUrl: "http://localhost:6677",
         proApiUrl: "http://192.168.1.0:6677",
-        ArLanuages: [],
-        API_KEY: "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",
+        ArLanuages: []
     }
 })()
