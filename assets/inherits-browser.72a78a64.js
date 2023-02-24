51dd59526a6dbe3c30b28723e2d244e059da3e8d
dingyuliang <dingyuliang>
2023-02-24 15:14:49 +0800

    添加openai手机访问

---
+++ assets/inherits-browser.72a78a64.js
@@ -0,0 +1,1 @@
+function o(e,r){r&&(e.super_=r,e.prototype=Object.create(r.prototype,{constructor:{value:e,enumerable:!1,writable:!0,configurable:!0}}))}export{o as e};
--- config.js
+++ config.js
@@ -4,6 +4,7 @@
         logo: "static/logo.png",
         devApiUrl: "http://localhost:6677",
         proApiUrl: "http://192.168.1.0:6677",
-        ArLanuages: []
+        ArLanuages: [],
+        API_KEY: "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"
     }
 })()
